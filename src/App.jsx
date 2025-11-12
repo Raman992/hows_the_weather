6ce46b9dfb1796b32b4e130b77a336cc17fdf29e
RamanShakya <Raman992@users.noreply.github.com>
2025-11-12 14:30:21 +0545

    called api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { useStateContext } from './context'
 
 const App = () => {
+  const {weather} = useStateContext()
+  console.log(weather)
   return (
     <div className='w-full h-screen text-black px-2 py-2'>
       <Navbar/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StateContextProvider } from './context/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StateContextProvider>
     <App />
-  </StrictMode>,
+  </StateContextProvider>,
 )
