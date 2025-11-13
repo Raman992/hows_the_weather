910f3261481c23ed55a3a32184c6533a68de2463
RamanShakya <Raman992@users.noreply.github.com>
2025-11-13 16:00:48 +0545

    updated navbar and weathercard to glass

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,15 @@ const Navbar = () => {
     }
 
   return (
-<nav className="w-full p-2 flex justify-between items-center ">
+<nav className="w-full p-2 flex justify-between items-center fixed top-0 left-0 right-0 z-50 bg-white/20 backdrop-blur-md border-b border-gray-200/50 ">
+    <img src="icons/icon.png" alt="logo" className='w-9' />
     <h1 className='font-bold tracking-wide text-3xl px-1'>How's the weather</h1>
     <div className='w-60 overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
         <i className="fa-solid fa-magnifying-glass "></i>
         <input type="text" placeholder='Search' onKeyUp={onKeyUp} className='focus:outline-0 w-full text-[#212121] text-lg' value={input} onChange={onChange}/>
     </div>
 </nav>
+
   )
 }
 
